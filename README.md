d82affcd45c7dd661b8f2bcccb8081897554eec4
ocean1208 <baiming1208@gmail.com>
2013-11-12 15:54:46 +0800

    write code on thinkpad.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 UsingGitHub
 ===========
+2013.11.12
+this java project is for learning how to use the github to save my code.
--- src/com/yiming/java/HelloGitHub.java
+++ src/com/yiming/java/HelloGitHub.java
@@ -5,6 +5,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello github, I am coming again.");
+		System.out.println("I am write code on my thinkpad.");
 	}
 
 }
